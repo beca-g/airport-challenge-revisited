549cdb6a6356554dedae0b4968117750af9d469d
Beca-g <rebekahgalliano@gmail.com>
2021-01-04 18:10:17 +0000

    Planes can take off for an airport when instructed
    Added take_off method to airport class
    
    As an air traffic controller
    So I can get passengers on the way to their destination
    I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,12 +1,23 @@
+require "Airport"
+require "Plane"
+
 describe "user stories" do
-  describe "user story one" do
-    # As an air traffic controller 
-    # So I can get passengers to a destination 
-    # I want to instruct a plane to land at an airport
-    it "so planes can land at airports, instruct a plane to land" do
-      airport = Airport.new
-      plane = Plane.new
-      expect { airport.land(plane) }.not_to raise_error
-    end
+
+  # As an air traffic controller 
+  # So I can get passengers to a destination 
+  # I want to instruct a plane to land at an airport
+  it "so planes can land at airports, instruct a plane to land" do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.land(plane) }.not_to raise_error
+  end
+
+  # As an air traffic controller 
+  # So I can get passengers on the way to their destination 
+  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+  it "so planes can take off from airports, instruct a plane to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
   end
 end
--- spec/units/airport_spec.rb
+++ spec/units/airport_spec.rb
@@ -7,4 +7,10 @@ describe Airport do
       expect(airport).to respond_to(:land).with(1).argument
     end
   end
+
+  describe "#take_off" do
+    it "instructs planes to take_off" do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
 end
