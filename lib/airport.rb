2f3e3e8e7001aba68171f03830c657a95de9c65c
Beca-g <rebekahgalliano@gmail.com>
2021-01-05 18:31:52 +0000

    Set a constant default capacity that can be overridden
    As the system designer
    So that the software can be used for many different airports
    I would like a default airport capacity that can be overridden as appropriate

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,15 @@
 class Airport
   attr_reader :capacity, :runway
+  DEFAULT_CAPACITY = 20
 
-  def initialize(capacity = 20)
+  def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
     @runway = []
   end
 
   def land(plane)
     raise "Unable to land: airport full." if @runway.length == @capacity
+    
     @runway << plane
   end
 
