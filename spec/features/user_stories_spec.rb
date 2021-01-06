19460a5f9c19c6f3cddfa79cc99f16acadc72a9b
Beca-g <rebekahgalliano@gmail.com>
2021-01-06 17:10:20 +0000

    Refactored feature tests

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,53 +1,53 @@
 describe "user stories" do
+  let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
 
-  # As an air traffic controller 
-  # So I can get passengers to a destination 
-  # I want to instruct a plane to land at an airport
-  it "so planes can land at airports, instruct a plane to land" do
-    airport = Airport.new
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return false
-    expect { airport.land(plane) }.not_to raise_error
+  context "when weather is not stomry" do
+    before do
+      allow(airport).to receive(:stormy?).and_return false
+    end
+
+    # As an air traffic controller 
+    # So I can get passengers to a destination 
+    # I want to instruct a plane to land at an airport
+    it "so planes can land at airports, instruct a plane to land" do
+      expect { airport.land(plane) }.not_to raise_error
+    end
+
+    # As an air traffic controller 
+    # So I can get passengers on the way to their destination 
+    # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    it "so planes can take off from airports, instruct a plane to take off" do
+      expect { airport.take_off(plane) }.not_to raise_error
+    end
+
+    # As an air traffic controller 
+    # To ensure safety 
+    # I want to prevent landing when the airport is full 
+    it "prevents planes landing when the airport is at capacity" do
+      20.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error "Unable to land: airport full."
+    end
+
+    # As the system designer
+    # So that the software can be used for many different airports
+    # I would like a default airport capacity that can be overridden as appropriate
+    it "airports have a default capacity" do
+      Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error "Unable to land: airport full."
+    end
   end
 
-  # As an air traffic controller 
-  # So I can get passengers on the way to their destination 
-  # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-  it "so planes can take off from airports, instruct a plane to take off" do
-    airport = Airport.new
-    plane = Plane.new
-    expect { airport.take_off(plane) }.not_to raise_error
-  end
+  context "when stormy" do
+    before do
+      allow(airport).to receive(:stormy?).and_return true
+    end
 
-  # As an air traffic controller 
-  # To ensure safety 
-  # I want to prevent landing when the airport is full 
-  it "prevents planes landing when the airport is at capacity" do
-    airport = Airport.new
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return false
-    20.times { airport.land(plane) }
-    expect { airport.land(plane) }.to raise_error "Unable to land: airport full."
-  end
-
-  # As the system designer
-  # So that the software can be used for many different airports
-  # I would like a default airport capacity that can be overridden as appropriate
-  it "airports have a default capacity" do
-    airport = Airport.new
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return false
-    Airport::DEFAULT_CAPACITY.times { airport.land(plane) }
-    expect { airport.land(plane) }.to raise_error "Unable to land: airport full."
-  end
-
-  # As an air traffic controller 
-  # To ensure safety 
-  # I want to prevent takeoff when weather is stormy 
-  it "does not allow planes to land when stormy" do
-    airport = Airport.new
-    plane = Plane.new
-    allow(airport).to receive(:stormy?).and_return true
-    expect { airport.land(plane) }.to raise_error "Unable to land: weather is stormy."
+    # As an air traffic controller 
+    # To ensure safety 
+    # I want to prevent takeoff when weather is stormy 
+    it "does not allow planes to land" do
+      expect { airport.land(plane) }.to raise_error "Unable to land: weather is stormy."
+    end
   end
 end
