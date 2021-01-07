757bd9f26a754b06a20dec16b4a1d1d0c2bf2fa3
Beca-g <rebekahgalliano@gmail.com>
2021-01-07 12:54:20 +0000

    Only landed planes are able to take off

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,2 +1,10 @@
 class Plane
+
+  def take_off
+    raise "Plane unable to take off: plane already flying."
+  end
+
+  def airport
+    raise "Plane not at airport: plane already flying."
+  end
 end
--- spec/units/plane_spec.rb
+++ spec/units/plane_spec.rb
@@ -1,5 +1,21 @@
-require "./lib/plane.rb"
+require "Plane"
 
 describe Plane do
-  
+  subject(:plane) { described_class.new }
+
+  describe "#take_off" do
+    it { is_expected.to respond_to :take_off }
+
+    it "raises an error if the plane is already flying" do
+      expect { plane.take_off }.to raise_error "Plane unable to take off: plane already flying."
+    end
+  end
+
+  describe "#airport" do
+    it { is_expected.to respond_to :airport }
+
+    it "raises an error if the plane is already flying" do
+      expect { plane.airport }.to raise_error "Plane not at airport: plane already flying."
+    end
+  end
 end
